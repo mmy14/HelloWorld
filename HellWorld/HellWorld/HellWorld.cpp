d64f7d061cd500cc3f5cdf18cd05d25c8d9b4084
Mikhail Medvedev <Mmy14@mail.ru>
2013-02-26 11:15:17 +0400

    llllll
    sdrhst

---
+++ HellWorld/HellWorld/HellWorld.cpp
@@ -0,0 +1,14 @@
+// HellWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello, World";
+	system("PAUSE");
+	return 0;
+}
+
